c9ec55472d7f9f5e714451cb637db692fac31944
Ajaysbox <ajaysbox@gmail.com>
2017-05-04 15:24:07 +0530

    1st router commit from Ajay

---
+++ src/app/aj-module-home/aj-module-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-aj-module-home',
+  templateUrl: './aj-module-home.component.html',
+  styleUrls: ['./aj-module-home.component.css']
+})
+export class AjModuleHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
